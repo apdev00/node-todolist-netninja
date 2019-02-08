9cf76d61c70171a49f6ccca93f213bf24abcf839
Alex Pasieka <alex.pasieka00@gmail.com>
2019-02-08 13:14:42 -0500

    chap34 20190208b

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,25 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'do laundry'}];
+
 module.exports = function(app) {
 
     app.get('/todo', function(req, res) {
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function(req, res) {
-
+    app.post('/todo', urlencodedParser, function(req, res) {
+        console.log(req.body);
+        data.push(req.body);
+        res.json({todos: data});
     });
 
-    app.delete('/todo', function(req, res) {
-
+    app.delete('/todo/:item', function(req, res) {
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json({todos: data});
     });
 
 };
