4b1009b745bd7071896e78fafa121a3c60c5fda3
Alex Pasieka <alex.pasieka00@gmail.com>
2019-02-11 21:19:58 -0500

    chap37 20190211b

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -11,36 +11,51 @@ var todoSchema = new mongoose.Schema({
 
 var TodoModel = mongoose.model('Todo', todoSchema);
 
-var itemOne = TodoModel({
-    item: 'get beer'
-}).save(function(err) {
-    if (err){
-        throw error;
-    }
-    console.log('item saved');
-});
+// var itemOne = TodoModel({
+//     item: 'get beer'
+// }).save(function(err) {
+//     if (err){
+//         throw error;
+//     }
+//     console.log('item saved');
+// });
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'do laundry'}];
+//var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'do laundry'}];
 
 module.exports = function(app) {
 
     app.get('/todo', function(req, res) {
-        res.render('todo', {todos: data});
+        // get data from mongodb and pass it to view
+        TodoModel.find({}, function(err, data){
+            if (err){
+                throw err;
+            }
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res) {
-        console.log(req.body);
-        data.push(req.body);
-        res.json({todos: data});
+        // get data from view and add it to mongodb
+        var newTodo = TodoModel(req.body).save(function(err, data){
+            if (err){
+                throw err;
+            }
+            //res.json(data);
+            res.render('todo', {todos: data});
+        });
     });
 
     app.delete('/todo/:item', function(req, res) {
-        data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
+        // delete the requested item from mongodb
+        TodoModel.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+            if (err){
+                throw err;
+            }
+            //res.json(data);
+            res.render('todo', {todos: data});
         });
-        res.json({todos: data});
     });
 
 };
